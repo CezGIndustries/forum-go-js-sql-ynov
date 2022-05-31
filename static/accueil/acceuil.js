1cbabea6dec0c0dc8031d59194867b8be0f9164d
scappe <sean.cappe@ynov.com>
2022-05-31 12:48:15 +0200

    recup

---
+++ static/accueil/acceuil.js
@@ -0,0 +1,25 @@
+let input = document.getElementById("postCron")
+let post = document.getElementById("posts")
+let id = 0
+
+input.addEventListener("keyup",function(event){
+    if(event.keyCode == 13){
+    id ++
+    let val = document.querySelector('input').value
+    document.querySelector('input').value = ''
+    document.querySelector('input').focus()
+    let Cron = document.createElement('div')
+    let data = document.createTextNode(val +" "+id)
+    Cron.setAttribute('class','cron')
+    Cron.setAttribute('onclick','displayCron()')
+    Cron.setAttribute('id',id)
+    Cron.appendChild(data)
+    post.after(Cron)
+    console.log(val + id)
+}
+});
+
+function displayCron(){
+    
+    console.log(event.srcElement.id)
+}
