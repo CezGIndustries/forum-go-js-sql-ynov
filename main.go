f85401e4f7996f512e521d1c119702012f9c9777
bvernet <benjamin.vernet@ynov.com>
2022-06-02 17:01:24 +0200

    Fonction login utilisateur

--- main.go
+++ main.go
@@ -1,27 +1,48 @@
 package main
 
 import (
+	"database/sql"
 	"forum/forum"
-	"log"
-	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
+type App struct {
+	Router *mux.Router
+	DB     *sql.DB
+}
+
+// func main() {
+// 	Env := App{
+// 		Router: mux.NewRouter(),
+// 		DB:     forum.DatabaseInit("forum/"),
+// 	}
+
+// 	Env.Router.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
+
+// 	Env.Router.HandleFunc("/", forum.Connexion_Creation())
+// 	Env.Router.HandleFunc("/home", forum.Home())
+
+// 	Env.Router.HandleFunc("/profil/{nameUser}", forum.Profil())
+
+// 	Env.Router.HandleFunc("/chronosdb/POST/{initUser}", forum.InitUser())
+
+// 	s := &http.Server{
+// 		Addr:    ":8080",
+// 		Handler: Env.Router,
+// 	}
+
+// 	log.Fatal(s.ListenAndServe())
+// }
+
 func main() {
-	m := mux.NewRouter()
-
-	m.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
-
-	m.HandleFunc("/", forum.Connexion_Creation())
-	m.HandleFunc("/home", forum.Home())
-
-	m.HandleFunc("/profil/{nameUser}", forum.Profil())
-
-	s := &http.Server{
-		Addr:    ":8080",
-		Handler: m,
+	Env := App{
+		Router: mux.NewRouter(),
+		DB:     forum.DatabaseInit("forum/"),
 	}
+	forum.CreateNewUser(Env.DB, forum.UserLogin{"azeaze", "CezGain@cez.gain", "azerty"})
+	forum.CreateNewUser(Env.DB, forum.UserLogin{"aze", "CezGain@cez.gain", "azerty"})
+	forum.CreateNewUser(Env.DB, forum.UserLogin{"CezGain", "CezGain@cez.gain", "azerty"})
 
-	log.Fatal(s.ListenAndServe())
+	forum.CheckUser(Env.DB, forum.UserLogin{"CezGain", "CezGain@cez.gain", "azerty"})
 }
