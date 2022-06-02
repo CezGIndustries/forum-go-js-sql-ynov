4badaac2595e1d60ae55288ef97d41230593fcd4
Jaleel <jaleel.aouadj@ynov.com>
2022-06-02 16:56:33 +0200

    version test

--- static/connexion-creation/index.js
+++ static/connexion-creation/index.js
@@ -1,6 +1,7 @@
 const connect = document.getElementById("loginform");
 const signup = document.getElementById("signupform");
 const mbg = document.getElementById("mainbuttongroup");
+const githubLogin = document.getElementById("login-github")
 
 function register() {
     connect.style.left = "-400px";
@@ -14,3 +15,23 @@ function login() {
     signup.style.left = "450px";
     mbg.style.left = "0";
 }
+
+githubLogin.onclick = function(){
+    var url = "https://api.github.com/user";
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url);
+    
+    xhr.setRequestHeader("Authorization", "Basic dXNlcm5hbWU6dG9rZW4=");
+    
+    xhr.onreadystatechange = function () {
+       if (xhr.readyState === 4) {
+          console.log(xhr.status);
+          console.log(xhr.responseText);
+       }};
+    
+    xhr.send();
+    console.log(xhr)
+    console.log("siuu")
+}
+console.log(githubLogin)
