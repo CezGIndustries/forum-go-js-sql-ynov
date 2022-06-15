573c240c5d976a8ccd86d08ea569eb2ccddd8206
Jaleel <jaleel.aouadj@ynov.com>
2022-06-15 16:46:44 +0200

    loggit

---
+++ forum/githublogin.go
@@ -0,0 +1,106 @@
+package forum
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+func loggedinHandler(w http.ResponseWriter, r *http.Request, githubData string) {
+	if githubData == "" {
+		// Unauthorized users get an unauthorized message
+		fmt.Fprintf(w, "UNAUTHORIZED!")
+		return
+	}
+
+	w.Header().Set("Content-type", "application/json")
+
+	// Prettifying the json
+	var prettyJSON bytes.Buffer
+	// json.indent is a library utility function to prettify JSON indentation
+	parserr := json.Indent(&prettyJSON, []byte(githubData), "", "\t")
+	if parserr != nil {
+		log.Panic("JSON parse error")
+	}
+
+	// Return the prettified JSON as a string
+	fmt.Fprintf(w, string(prettyJSON.Bytes()))
+}
+
+func getGithubData(accessToken string) string {
+	req, reqerr := http.NewRequest("GET", "https://api.github.com/user", nil)
+	if reqerr != nil {
+		log.Panic("API Request creation failed")
+	}
+
+	authorizationHeaderValue := fmt.Sprintf("token %s", accessToken)
+	req.Header.Set("Authorization", authorizationHeaderValue)
+
+	resp, resperr := http.DefaultClient.Do(req)
+	if resperr != nil {
+		log.Panic("Request failed")
+	}
+
+	respbody, _ := ioutil.ReadAll(resp.Body)
+
+	return string(respbody)
+}
+
+func getGithubAccessToken(code string) string {
+
+	clientID := getGithubClientID()
+	clientSecret := getGithubClientSecret()
+
+	requestBodyMap := map[string]string{"client_id": clientID, "client_secret": clientSecret, "code": code}
+	requestJSON, _ := json.Marshal(requestBodyMap)
+
+	req, reqerr := http.NewRequest("POST", "https://github.com/login/oauth/access_token", bytes.NewBuffer(requestJSON))
+	if reqerr != nil {
+		log.Panic("Request creation failed")
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Accept", "application/json")
+
+	resp, resperr := http.DefaultClient.Do(req)
+	if resperr != nil {
+		log.Panic("Request failed")
+	}
+
+	respbody, _ := ioutil.ReadAll(resp.Body)
+
+	// Represents the response received from Github
+	type githubAccessTokenResponse struct {
+		AccessToken string `json:"access_token"`
+		TokenType   string `json:"token_type"`
+		Scope       string `json:"scope"`
+	}
+
+	var ghresp githubAccessTokenResponse
+	json.Unmarshal(respbody, &ghresp)
+
+	return ghresp.AccessToken
+}
+
+func getGithubClientID() string {
+
+	githubClientID, exists := os.LookupEnv("479d0f7a478fe8897cd7")
+	if !exists {
+		log.Fatal("Github Client ID not defined in .env file")
+	}
+
+	return githubClientID
+}
+
+func getGithubClientSecret() string {
+
+	githubClientSecret, exists := os.LookupEnv("d9796ad261121e9a02a51cf2c154f286a54b17be")
+	if !exists {
+		log.Fatal("Github Client ID not defined in .env file")
+	}
+
+	return githubClientSecret
+}
